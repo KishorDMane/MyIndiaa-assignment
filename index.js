b4e5cdcb3d19affa893afc276c8a0e1136611a18
KishorDMane <manekishor212@gmail.com>
2024-06-19 09:45:23 +0530

    new server test

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const express= require('express')
-const app = express();
-
-
-
-app.use(express.json());
-
-
- 
-app.get("/get", (req,res)=>{
-res.send("started on 8000")
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-
-
-app.listen(8000 , ()=>{
-console.log("on 8000");
-} )
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
